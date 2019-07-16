f307b19578c92a3ecfb94e8ecbe07948fc5bd2d7
Sem8 <mssemmi8@gmail.com>
2019-07-16 00:20:55 -0400

    bubble sort test ran

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,35 +1,23 @@
-# # TO-DO: Complete the selection_sort() function below 
-# def selection_sort( arr ):
-#     # loop through n-1 elements
-#     for i in range(0, len(arr) - 1):
-#         cur_index = i
-#         smallest_index = cur_index
-#         # TO-DO: find next smallest element
-#         # (hint, can do in 3 loc) 
-             
-
-
-
+# TO-DO: Complete the selection_sort() function below 
+def selection_sort( arr ):
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc)
+        if arr[smallest_index] < arr[i]:
+            arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
         # TO-DO: swap
-
-
-
-
-    # return arr
+    return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for i in range(0, len(arr)):
-        # temp = 0
-        an_element = arr[i]        
-        neighbor_element = arr[i+1]
-        if an_element == neighbor_element or an_element < neighbor_element:
-            pass
-        elif an_element > neighbor_element:
-            temp = an_element
-            an_element = neighbor_element
-            neighbor_element = temp            
+    for i in range(0, len(arr) - 1): 
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]    
     return arr
 
 
